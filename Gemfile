ead729db7b798c75b9c66a825b0a313cfd9050e0
Peter Okorafor <peterdtitan@gmail.com>
2023-07-19 07:15:30 +0300

    Fix linters

--- Gemfile
+++ Gemfile
@@ -72,4 +72,4 @@ end
 
 gem 'tailwindcss-rails', '~> 2.0'
 
-gem "devise", "~> 4.9"
+gem 'devise', '~> 4.9'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-    skip_before_action :authenticate_user!, only: [:index]
-  
-    def index
-      redirect_to categories_path if user_signed_in?
-    end
+  skip_before_action :authenticate_user!, only: [:index]
+
+  def index
+    redirect_to categories_path if user_signed_in?
   end
+end
